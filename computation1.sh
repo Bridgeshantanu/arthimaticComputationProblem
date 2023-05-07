d008c7c8b4502d351d26a66d8b594c71206eac58
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-05-07 17:10:48 +0530

    Add: next computation done

--- computation1.sh
+++ computation1.sh
@@ -1,9 +1,13 @@
+
 read -p "Enter value for a:" a
 
 read -p "Enter value for b:" b
 
 read -p "Enter value for c:" c
 
-result=$((a + b * c))
+result1=$((a + b * c))
+
+partTimeHour=8
+result2=$((a * b + c * partTimeHour))
 
 
