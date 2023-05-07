1f60962a66c9bdcd2daa33719a599b398865c74f
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-05-07 16:50:53 +0530

    Add: computation done

--- computation1.sh
+++ computation1.sh
@@ -1,6 +1,9 @@
-read -p "Enter value for a:" num
+read -p "Enter value for a:" a
 
-read -p "Enter value for b:" num
+read -p "Enter value for b:" b
 
-read -p "Enter value for c:" num
+read -p "Enter value for c:" c
+
+result=$((a + b * c))
+
 
